a82ade852ed26c3d15a88c7b8ea5b420f05df73d
Rakesh Chauhan <devs.neeraj86@gmail.com>
2024-01-16 14:52:35 +0530

    added logging

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace mongodb_dotnet_example
 {
@@ -13,7 +11,34 @@ namespace mongodb_dotnet_example
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+             .ReadFrom.Configuration(new ConfigurationBuilder()
+                 .AddJsonFile("appsettings.json")
+                 .Build())
+             .WriteTo.Console()
+             .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
+             .CreateLogger();
+
+            Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
+
+            try
+            {
+                if (Log.IsEnabled(Serilog.Events.LogEventLevel.Information))
+                    Log.Information("Starting api");
+
+                CreateHostBuilder(args).Build().Run();
+                
+                if (Log.IsEnabled(Serilog.Events.LogEventLevel.Information))
+                    Log.Information("Api Started");
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -21,6 +46,7 @@ namespace mongodb_dotnet_example
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+            .UseSerilog();
     }
 }
